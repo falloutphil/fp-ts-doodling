31db2ca0b1000bd09e230c26dab08a7fe1b6ab6d
Phil Beadling <phil.beadling@opetek.io>
2024-09-01 09:43:45 +0100

    remove deprecated code and use contramap

--- monoid-ordering.ts
+++ monoid-ordering.ts
@@ -1,7 +1,8 @@
-import { ordNumber, ordString, fromCompare } from 'fp-ts/lib/Ord';
-import { concatAll } from 'fp-ts/lib/Monoid';
+import { Ord, fromCompare, contramap } from 'fp-ts/lib/Ord';
 import { Monoid as O, Ordering } from 'fp-ts/lib/Ordering';
-
+import { concatAll } from 'fp-ts/lib/Monoid';
+import * as N from 'fp-ts/lib/number';
+import * as S from 'fp-ts/lib/string';
 
 // lengthCompare :: String -> String -> Ordering
 // lengthCompare x y = (length x `compare` length y) `mappend`
@@ -9,28 +10,22 @@ import { Monoid as O, Ordering } from 'fp-ts/lib/Ordering';
 //                     (x `compare` y)
 //     where vowels = length . filter (`elem` "aeiou")
 
-
-
 // Utility function to count vowels in a string
 const countVowels = (s: string): number =>
   s.split('').filter(c => 'aeiou'.indexOf(c.toLowerCase()) !== -1).length;
 
 // Ord instance for comparing string lengths
-const ordLength = fromCompare<string>((x, y) => 
-  ordNumber.compare(x.length, y.length)
-);
+const ordLength = contramap((s: string) => s.length)(N.Ord);
 
 // Ord instance for comparing the number of vowels
-const ordVowels = fromCompare<string>((x, y) => 
-  ordNumber.compare(countVowels(x), countVowels(y))
-);
+const ordVowels = contramap((s: string) => countVowels(s))(N.Ord);
 
 // Combining the ordering using concatAll
 const lengthCompare = (x: string, y: string): Ordering =>
   concatAll(O)([
     ordLength.compare(x, y),
     ordVowels.compare(x, y),
-    ordString.compare(x, y),
+    S.Ord.compare(x, y),
   ]);
 
 // Testing the lengthCompare function
